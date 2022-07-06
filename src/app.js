0a7ad228f68b358a2c718b84ab25d514e9daed46
Kate <neilsenk09@gmail.com>
2022-07-06 14:09:21 -0400

    add notFound and ErrorHandler files

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
 
 //Not found handler
-app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
+app.use(notFound);
 
 //Error Handler
-app.use((error, req, res, next) => {
-  console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
-});
+app.use(errorHandler);
 
 module.exports = app;
--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -11,19 +11,6 @@ async function reviewExists(req, res, next) {
   next({ status: 404, message: `Review cannot be found` });
 }
 
-//middleware to check for content
-function hasContent(req, res, next) {
-  console.log(req.body.data);
-  const { data: { content } = {} } = req.body;
-  console.log(content);
-  if (content) {
-    res.locals.content = content;
-    return next();
-  } else {
-    next({ status: 400, message: `Must include content` });
-  }
-}
-
 async function update(req, res) {
   const updatedReview = {
     ...req.body.data,
@@ -32,15 +19,6 @@ async function update(req, res) {
   const data = await service.update(updatedReview);
   res.json({ data });
 }
-
-// async function update(req, res) {
-//   const updatedReview = {
-//     ...req.body.data,
-//     review_id: res.locals.review.review_id,
-//   };
-//   const data = await service.update(updatedReview);
-//   res.json({ data });
-// }
 
 async function destroy(req, res) {
   const { review } = res.locals;
